26fca6b04419455e33ddcb4cceb2fe75be7d2e8e
macchoudhary <macchoudhary>
2016-09-18 17:03:15 +0530

    Add project files.

---
+++ Expense/Expense/Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Expense
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            titleLabel.TextColor = Color.FromHex("#469978");
+            titleLabel.HorizontalOptions = LayoutOptions.Start;
+            titleLabel.VerticalOptions = LayoutOptions.Center;
+            displayLabel.Text = "";
+
+        }
+        private void OnBackspaceButtonClicked(object sender, EventArgs e)
+        {
+            if(displayLabel.Text.Length>1)
+            displayLabel.Text = displayLabel.Text.Remove(displayLabel.Text.Length - 1);
+        }
+        private void OnDigitButtonClicked(object sender, EventArgs e)
+        {
+            if (displayLabel.Text.Length <4)
+                displayLabel.Text = displayLabel.Text + (sender as Button).Text;
+        }
+        private void OnValidateClicked(object sender, EventArgs e)
+        {
+            if ((sender as Button).Text.Equals("OK"))
+                displayLabel.Text = "Success";
+            else
+                displayLabel.Text = "Failure";
+        }
+    }
+    
+}
---
+++ Expense/Expense/Splash.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Expense
+{
+    public class Splash : ContentPage
+    {
+        public Splash()
+        {
+            Button skip = new Button
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                BackgroundColor = Xamarin.Forms.Color.Aqua,
+                Text = "Skip to proceed",
+                TextColor = Xamarin.Forms.Color.White,
+                VerticalOptions = LayoutOptions.End
+            };
+            skip.Clicked += OnButtonClicked;
+
+            Content = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                InputTransparent = true,
+
+                Children = {
+                     new Label { Text = "Expense Manager",
+                    HorizontalOptions = LayoutOptions.Center,
+                    FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                         FontAttributes = FontAttributes.Bold ,
+                    VerticalOptions = LayoutOptions.Start
+                    },
+                  new Image {
+                    HorizontalOptions = LayoutOptions.Center,
+                      VerticalOptions = LayoutOptions.Center,
+                     Source="monitor.png"
+                  },
+                     skip
+                }
+
+            };
+            Padding = Device.OnPlatform<Thickness>(new Thickness(20, 100, 20, 20), new Thickness(0), new Thickness(0));
+
+        }
+
+    async void OnButtonClicked(object sender, EventArgs args)
+        {         // Add Label to scrollable StackLayout.         
+            await Navigation.PushModalAsync(new Login());
+        }
+    }
+}
