5a533eb2c45deae364cd63825e7e76859cf45d88
macchoudhary <macchoudhary>
2016-09-21 14:38:19 +0530

    Commit

--- Expense/Expense/Dashboard.xaml.cs
+++ Expense/Expense/Dashboard.xaml.cs
@@ -13,6 +13,17 @@ namespace Expense
         public Dashboard()
         {
             InitializeComponent();
+            masterPage.ListView.ItemSelected += OnItemSelected;
+        }
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as DrawerItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                masterPage.ListView.SelectedItem = null;
+                IsPresented = false;
+            }
         }
     }
 }
--- Expense/Expense/MasterPage.xaml.cs
+++ Expense/Expense/MasterPage.xaml.cs
@@ -22,14 +22,45 @@ namespace Expense
            
             masterPageItems.Add(new DrawerItem
             {
-                Title = "Contacts",
-                IconSource = "profile.png"
+                Title = "Current Feeds",
+                IconSource = "dashboard.png",
+                TargetType = typeof(Feed)
             });
            masterPageItems.Add(new DrawerItem
             {
-                Title = "Logout",
-                IconSource = "profile.png"
+                Title = "Expense Details",
+                IconSource = "check.png"
            });
+            masterPageItems.Add(new DrawerItem
+            {
+                Title = "Add Expense",
+                IconSource = "add.png"
+            });
+            masterPageItems.Add(new DrawerItem
+            {
+                Title = "Cash Wallet",
+                IconSource = "wallet.png"
+            });
+            masterPageItems.Add(new DrawerItem
+            {
+                Title = "Categories",
+               IconSource = "category.png"
+            });
+            masterPageItems.Add(new DrawerItem
+            {
+                Title = "Expense Stats",
+                IconSource = "expense.png"
+            });
+            masterPageItems.Add(new DrawerItem
+            {
+                Title = "Set Alert",
+                IconSource = "warning.png"
+            });
+            masterPageItems.Add(new DrawerItem
+            {
+                Title = "Compare Expense",
+                IconSource = "comparison.png"
+            });
             listView.ItemsSource = masterPageItems;
         }
 
