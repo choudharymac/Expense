a603af897b5b3e106fa8d9a2aa4d3c161840b579
macchoudhary <macchoudhary>
2016-09-22 19:46:54 +0530

    Commit

---
+++ Expense/Expense/AlertDrawerItems.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expense
+{
+    class AlertDrawerItems
+    {
+        public string Alert { get; set; }
+
+        public string IconSource { get; set; }
+
+        public string Amount { get; set; }
+
+        public int TargetType { get; set; }
+    }
+}
--- Expense/Expense/SetAlert.xaml.cs
+++ Expense/Expense/SetAlert.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,31 @@ namespace Expense
 {
     public partial class SetAlert : ContentPage
     {
+        public ListView ListView { get { return listView; } }
+
+        ObservableCollection<AlertDrawerItems> masterPageItems = new ObservableCollection<AlertDrawerItems>();
+
         public SetAlert()
         {
             InitializeComponent();
+
+          masterPageItems.Add(new AlertDrawerItems
+            {
+                Alert = "Low Balance",
+                Amount="30 USD",
+                IconSource = "bell.png",
+            });
+            listView.ItemsSource = masterPageItems;
+        }
+        private void OnAddAlert(object sender, EventArgs args)
+        {
+          masterPageItems.Add(new AlertDrawerItems
+            {
+                Alert = message.Text,
+                Amount = alertamount .Text+ " USD",
+                IconSource = "bell.png",
+            });
+            listView.ItemsSource = masterPageItems;
         }
     }
 }
