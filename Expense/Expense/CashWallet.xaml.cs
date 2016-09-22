cc0e83a00c386c0928f321e0a8feaeed4365cf15
macchoudhary <macchoudhary>
2016-09-22 13:47:46 +0530

    Commit

--- Expense/Expense/CashWallet.xaml.cs
+++ Expense/Expense/CashWallet.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Android.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,14 @@ namespace Expense
         public CashWallet()
         {
             InitializeComponent();
+            amountleft.Text ="0" + " AUD";
         }
+        private void addmore(object sender, EventArgs args)
+        {
+            int sum = Int32.Parse(amountleft.Text.Substring(0, amountleft.Text.Length - 4))+ Int32.Parse(wallet.Text);
+            amountleft.Text = sum + " AUD";
+            wallet.Text = "";
+            moneywallet.IsVisible = false;
+       }
     }
 }
